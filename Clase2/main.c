555504d12cb467e5c87ee6e5c53ce2fa080f5e91
LucasRuggi <47321324+LucasRuggi@users.noreply.github.com>
2019-03-19 11:49:13 -0300

    Add files via upload

---
+++ Clase2/main.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//while, cuando no sabemos cuantas repeticiones queremos
+//for cuando sabemos cuantas repeticiones tenemos
+
+int main()
+{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   /* int i;
+    for(i=0;i<10;i++){
+        printf("%d\t", i+1);
+    }*/
+
+
+  /*int i=0;
+  while(i<10){
+
+       printf("%d\t", i+1);
+    i++;
+  }
+
+
+   /*
+    char palabra[15];//siempre hay que poner uno mas de los caracteres que queremos ej: si queremos 14 se pone 15
+
+    printf("Ingrese una palabra: ");
+    sacnf("%s", palabra);
+    printf("Usted ingreso la palabra: %s",palabra);
+
+    int num1;
+    int num2;
+    int total;
+    float promedio;
+
+    printf("Ingrese un numero: ");
+    scanf("%d",&num1);
+    printf("Ingrese otro numero: ");
+    scanf("%d",&num2);
+    total = num1 + num2;
+    promedio = (float)total/2;
+    printf("El promedio es: %.2f\n", promedio);
+    printf("La suma es: %d",total);*/
+
+    return 0;
+}
