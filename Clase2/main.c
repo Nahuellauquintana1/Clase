785b2be748dcc1147e2cd67cfc554532d055398f
LucasRuggi <47321324+LucasRuggi@users.noreply.github.com>
2019-03-19 12:22:22 -0300

    Add files via upload

--- Clase2/main.c
+++ Clase2/main.c
@@ -4,10 +4,63 @@
 //while, cuando no sabemos cuantas repeticiones queremos
 //for cuando sabemos cuantas repeticiones tenemos
 
+//Se ingresan numeros enteros hasta que el usuario quiera que sean distintos de 0
+//informar cantida de numeros ingresados
+//cantidad de pares
+//Suma de positivos
+//Promedio de los negativos
+//Maximo y minimo
+// de los positivos el mas chico
+//y de los negativos el mas grande
+//cantidad de numeros que se encuentran entre el -10 y 50
+//promedio total
+
 int main()
 {
 
+int numero;
+char respuesta;
+int numeroIngresados = 0;
+int pares = 0;
+int positivo = 0;
+int negativo = 0;
+int sumaNegativo = 0;
+float promedioNeg;
+int flag = 0;
+int maximo;
+int minimo;
+int minPositivo;
+int maxNegativo;
 
+do{
+    printf("Ingrese un numero: ");
+    scanf("%d", &numero);
+    while(numero == 0){
+        printf("Error. Reingrese un numero: ");
+    }
+    numeroIngresados++;
+
+    if(numero%2 == 0){
+            pares++;
+    }
+    if(numero>0){
+        positivo++;
+    }else{
+        sumaNegativo=+negativo;
+        negativo++;
+    }
+    if(numero>maximo || flag == 0){
+        maximo = numero;
+    }
+    if(numero<minimo || flag == 0){
+        minimo = numero;
+        flag = 1;
+    }
+
+
+
+
+}while(respuesta == 's')
 
 
 
