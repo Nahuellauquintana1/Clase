da75f5c5ff1099098b38331748da82387558ebdb
Nahuel Quintana <47321353+Nahuellauquintana1@users.noreply.github.com>
2019-05-27 12:18:37 -0300

    Add files via upload

--- clase 18/Archivos binarios/main.c
+++ clase 18/Archivos binarios/main.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+typedef struct
+{
+    int legajo;
+    char nombre[64];
+    float sueldo;
+}eEmpleado;
+
+
+
+
 
 int main()
 {
@@ -9,7 +19,8 @@ int main()
     archivoBin = fopen("C:\\Users\\alumno\\Desktop\\Archivos binarios\\archivoBin","wb");
     fwrite(x, sizeof(int),1 , archivoBin);
     fclose(archivoBin);*/
-
+//crear de forma dinamica un emleado, una funcion se va a enfcargar de recibir un empleado y lo va a guardar en un arhcivo
+//de forma binaria, otara funcin lo lee, last 2 funtions reciben y muestran el resto in text. respectivamente
 
     int* x;
     x =(int*)malloc(sizeof(int));
